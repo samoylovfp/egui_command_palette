e055bd19cdcf45bb1252d0d26ccb55a746979027
Natalia Samoilova <samoilova.natalia.n@gmail.com>
2022-07-04 21:04:06 +0300

    Add command palette open and render methods

--- examples/text_editor.rs
+++ examples/text_editor.rs
@@ -1,17 +1,35 @@
+use egui::{Key, Modifiers};
+use egui_command_palette::CommandPalette;
+
 struct Editor {
     text: String,
+    command_palette: CommandPalette,
 }
 
 impl eframe::App for Editor {
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        egui::TopBottomPanel::top("panel").show(ctx, |ui|{
+        if ctx.input_mut().consume_key(
+            Modifiers {
+                ctrl: true,
+                shift: true,
+                ..Modifiers::NONE
+            },
+            Key::P,
+        ) {
+            self.command_palette.open(ctx.memory().focus());
+        }
+        self.command_palette.render(ctx);
+        egui::TopBottomPanel::top("panel").show(ctx, |ui| {
             if ui.button("uppercase").clicked() {
-                self.text = self.text.chars().flat_map(|c|c.to_uppercase()).collect();
+                self.text = self.text.chars().flat_map(|c| c.to_uppercase()).collect();
             }
         });
-        egui::CentralPanel::default().show(ctx, |ui|
-			ui.add_sized(ui.available_size(), egui::TextEdit::multiline(&mut self.text))
-		);
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.add_sized(
+                ui.available_size(),
+                egui::TextEdit::multiline(&mut self.text),
+            )
+        });
     }
 }
 
@@ -22,6 +40,7 @@ fn main() {
         Box::new(|ui| {
             Box::new(Editor {
                 text: String::from("hello"),
+                command_palette: CommandPalette::new(),
             })
         }),
     );
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+use egui::{Id, Key};
+
+pub struct CommandPalette {
+    is_open: bool,
+    search_term: String,
+    prev_element_focus: Option<Id>,
+}
+
+impl CommandPalette {
+    pub fn new() -> Self {
+        CommandPalette {
+            is_open: false,
+            search_term: String::new(),
+            prev_element_focus: None,
+        }
+    }
+}
+
+impl CommandPalette {
+    /// Opens the search for commands window
+    pub fn open(&mut self, prev_element_focus: Option<Id>) {
+        if !self.is_open {
+            self.prev_element_focus = dbg!(prev_element_focus);
+        }
+        self.is_open = true;
+    }
+    /// Renders the search for command window
+    pub fn render(&mut self, ctx: &egui::Context) {
+        if !self.is_open {
+            return;
+        }
+        egui::Window::new("command palette")
+            .collapsible(false)
+            .show(ctx, |ui| {
+                if ui.input().key_pressed(Key::Escape) {
+                    self.is_open = false;
+                }
+
+                ui.label("Type the command");
+                let search_input = ui.text_edit_singleline(&mut self.search_term);
+                if self.is_open {
+                    search_input.request_focus();
+                } else if let Some(id) = self.prev_element_focus {
+                    ctx.memory().request_focus(id);
+                }
+            });
+    }
 }
