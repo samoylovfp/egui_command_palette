be4df72f4c62757538c44e93b031c2c410e4d4ca
Natalia Samoilova <samoilova.natalia.n@gmail.com>
2022-07-07 17:01:56 +0300

    Add action filtering and activation

--- examples/text_editor.rs
+++ examples/text_editor.rs
@@ -1,10 +1,13 @@
 use egui::{Key, Modifiers};
-use egui_command_palette::CommandPalette;
+use egui_command_palette::{Action, CommandPalette};
 
 struct Editor {
     text: String,
     command_palette: CommandPalette,
 }
+
+const UPPERCASE_COMMAND: &str = "uppercase";
+const LOWERCASE_COMMAND: &str = "lowercase";
 
 impl eframe::App for Editor {
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
@@ -21,15 +24,28 @@ impl eframe::App for Editor {
         self.command_palette.render(ctx);
         egui::TopBottomPanel::top("panel").show(ctx, |ui| {
             if ui.button("uppercase").clicked() {
-                self.text = self.text.chars().flat_map(|c| c.to_uppercase()).collect();
+                self.command_palette.activate(UPPERCASE_COMMAND)
+            }
+            if ui.button("lowercase").clicked() {
+                self.command_palette.activate(LOWERCASE_COMMAND)
             }
         });
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.add_sized(
                 ui.available_size(),
                 egui::TextEdit::multiline(&mut self.text),
-            )
+            );
+            ui.collapsing("test", |ui| println!("Inside collapsing is called"));
         });
+
+        // FIXME: Switch to match
+        if self.command_palette.is_activated(UPPERCASE_COMMAND) {
+            self.text = self.text.chars().flat_map(|c| c.to_uppercase()).collect()
+        }
+        if self.command_palette.is_activated(LOWERCASE_COMMAND) {
+            self.text = self.text.chars().flat_map(|c| c.to_lowercase()).collect()
+        }
+        self.command_palette.reset_activations();
     }
 }
 
@@ -40,7 +56,16 @@ fn main() {
         Box::new(|ui| {
             Box::new(Editor {
                 text: String::from("hello"),
-                command_palette: CommandPalette::new(),
+                command_palette: CommandPalette::new(vec![
+                    Action::new(
+                        String::from(UPPERCASE_COMMAND),
+                        String::from("Make all the text uppercase"),
+                    ),
+                    Action::new(
+                        String::from(LOWERCASE_COMMAND),
+                        String::from("Make all the text lowercase"),
+                    ),
+                ]),
             })
         }),
     );
--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,84 @@
+//! disrupts the way you write actions
+//! Unfortunately
+//! the palette cannot activate a piece of code
+//! that is written in a hidden widget
+//! since the "ui" function of it is never called
+
+use std::{collections::HashSet, hash::Hash};
+
 use egui::{Id, Key};
 
 pub struct CommandPalette {
     is_open: bool,
     search_term: String,
     prev_element_focus: Option<Id>,
+    actions: HashSet<Action>,
+    recently_activated: HashSet<String>,
+}
+
+pub struct Action {
+    command: String,
+    description: String,
+}
+
+impl Action {
+    pub const fn new(command: String, description: String) -> Self {
+        Action {
+            command,
+            description,
+        }
+    }
+}
+
+impl PartialEq for Action {
+    fn eq(&self, other: &Self) -> bool {
+        self.command == other.command
+    }
+}
+
+impl Eq for Action {}
+
+impl Hash for Action {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.command.hash(state);
+    }
 }
 
 impl CommandPalette {
-    pub fn new() -> Self {
+    pub fn new(actions: Vec<Action>) -> Self {
         CommandPalette {
             is_open: false,
             search_term: String::new(),
             prev_element_focus: None,
+            actions: actions.into_iter().collect(),
+            recently_activated: HashSet::new(),
         }
+    }
+
+    pub fn activate(&mut self, action: &str) {
+        self.recently_activated.insert(action.to_string());
+    }
+    pub fn is_activated(&self, action: &str) -> bool {
+        self.recently_activated.contains(action)
+    }
+    pub fn reset_activations(&mut self) {
+        self.recently_activated.clear();
     }
 }
 
 impl CommandPalette {
     /// Opens the search for commands window
-    pub fn open(&mut self, prev_element_focus: Option<Id>) {
+    /// You can use `ctx.memory().focus()` to make commandpalette return focus
+    /// after it is done
+    pub fn open(&mut self, return_focus_to: Option<Id>) {
         if !self.is_open {
-            self.prev_element_focus = dbg!(prev_element_focus);
+            self.prev_element_focus = dbg!(return_focus_to);
         }
         self.is_open = true;
     }
     /// Renders the search for command window
     pub fn render(&mut self, ctx: &egui::Context) {
+        let mut activated_this_render = HashSet::new();
         if !self.is_open {
             return;
         }
@@ -43,6 +96,34 @@ impl CommandPalette {
                 } else if let Some(id) = self.prev_element_focus {
                     ctx.memory().request_focus(id);
                 }
+
+                egui::Grid::new("palette_action_list")
+                    .striped(true)
+                    .num_columns(1)
+                    .show(ui, |ui| {
+                        for action in &self.actions {
+                            if !action.command.contains(&self.search_term) {
+                                continue;
+                            }
+                            let action_clicked = ui
+                                .vertical(|ui| {
+                                    let b = ui.button(&action.command);
+                                    ui.label(&action.description);
+                                    b.clicked()
+                                })
+                                .inner;
+                            if action_clicked {
+                                activated_this_render.insert(action.command.clone());
+                                if let Some(w) = self.prev_element_focus {
+                                    ctx.memory().request_focus(w);
+                                }
+                                self.is_open = false;
+                            }
+                            ui.end_row()
+                        }
+                    })
             });
+        self.recently_activated.extend(activated_this_render);
     }
+    // pub fn register(&mut self, command: &str, description: &str) {}
 }
